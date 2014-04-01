49033be76cceed23b61e066854b46a1c10825b63
mark wilkinson <markw@illuminae.com>
2014-04-01 12:04:48 +0200

    added documentation and usage simplification

--- register_services.pl
+++ register_services.pl
@@ -3,20 +3,39 @@
 # an example of how to register a service
 # curl http://sadiframework.org/registry/register/ --data-urlencode 'serviceURI=http://sadiframework.org/examples/hello'
 
-my %servicenames;
-my $service_script = "http://biordf.org/cgi-bin/SADI/Bio2RDF2SADI/SADI/";  # trailing slash
+my $USAGE = <<EOUSAGE;
 
 
-opendir (DIR, "/home/biordf/public_html/ontologies/") || die "can't open ontologies folder";
+   usage:
+   
+       register_services.pl  http://url.to/service/script/SADI/   /path/to/config/files/
+       
+       NOTE:
+        - the service script needs a trailing slash
+	- the local path to the config files; ends before the Bio2RDF namespace
+	  e.g. /path/to/config/files/ [atlas/term_regulates_term.cfg]
+
+       
+EOUSAGE
+
+my $service_script;
+my $config_path;
+
+($service_script = $ARGV[0]) || die $USAGE;
+($config_path = $ARGV[0]) || die $USAGE;
+
+opendir (DIR, "$config_path") || die "can't open configuration folder";
 my @endpoints = readdir DIR;
 
-foreach my $end(@endpoints){
+
+my %servicenames;
+foreach my $end(@endpoints){  # we will use the filenames to create a non-redundant list of all possible services
 	next if $end =~ /^\./;
-	opendir (DIR2, "/home/biordf/public_html/ontologies/$end") || die "can't open sub-ontology folder";
+	opendir (DIR2, "$config_path/$end") || die "can't open sub-ontology folder";
 	my @services = readdir DIR2;
 	foreach my $servname(@services){
 		next if $servname =~ /^\./;
-		die unless ($servname =~ /(\S+)\.\S+/);
+		die "\n\nconfig filename is odd...$servname???\n\n" unless ($servname =~ /(\S+)\.\S+/);
 		$servname = $1; 
 		$servicenames{"$end/$servname"} = 1; # rely on hash to remove duplicates with .owl, .sparql, and .cfg extensions
 	}
@@ -28,7 +47,7 @@ closedir DIR2;
 my $count;
 
 foreach my $name(keys %servicenames){
-	# $name = "affymetrix/Probeset_inDataset_Dataset"; 
+	# e.g. $name = "affymetrix/Probeset_inDataset_Dataset"; 
 	print ++$count;
 	print "  curl http://sadiframework.org/registry/register/ --data-urlencode 'serviceURI=$service_script$name'\n";
 	my $fail =0;
@@ -37,8 +56,8 @@ foreach my $name(keys %servicenames){
 		open(REGISTER, "-|", "curl http://sadiframework.org/registry/register/ --data-urlencode 'serviceURI=$service_script$name'");
 		my @lines = <REGISTER>;
 		my $response = join ("", @lines);
-		# print $response;
-		if ($response =~ /\<h3\>success/is){
+
+		if ($response =~ /\<h3\>success/is){  # the result of this call is, unfortunately, HTML :-(
 			print "Successful!\n\n";
 			$fail = 5;
 		} elsif ($response =~ /error.*?blockquote>([^\<]+)/is){
@@ -62,5 +81,22 @@ foreach my $name(keys %servicenames){
 		sleep 3;  # give the registry a break :-)  poor Tomcat...
 	}
 
-	# exit 1;
+	# exit 1;   # if you just want to try one to see if it works, exit here
 }
+
+=head1  NAME
+
+ register_services
+
+
+=head1 USAGE
+   
+       register_services.pl  http://url.to/service/script/SADI/   /path/to/config/files/
+       
+       NOTE:
+        - the service script needs a trailing slash
+	- the local path to the config files; ends before the Bio2RDF namespace
+	  e.g. /path/to/config/files/ [atlas/term_regulates_term.cfg]
+
+=cut
+
